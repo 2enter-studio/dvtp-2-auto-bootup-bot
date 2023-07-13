01655b19829141749aa2232fc9d1ac9c4fed2236
dovilivob <david.iii.chiu@gmail.com>
2023-07-14 05:26:24 +0800

    unreal_play() test success

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,33 @@
 import robot from 'robotjs';
-robot.keyToggle("a", 'down'); 
 
+robot.setKeyboardDelay(300);
 
+async function sleep(ms: number) {
+  await new Promise(r => setTimeout(r, ms));
+}
+
+function key_binding(keys: Array<string>) {
+  for (const key of keys) {
+    robot.keyToggle(key, 'down')
+  }
+  for (const key of keys) {
+    robot.keyToggle(key, 'up')
+  }
+}
+
+function unreal_play() {
+  key_binding(['alt', 'p'])
+}
+
+const {width, height} = robot.getScreenSize();
+
+// await sleep(4000);
+
+// console.log('pressed')
+// unreal_play();
+
+console.log(width, height)
+for (let i = 0; i < 10; i++){
+  await sleep(500)
+  console.log(robot.getMousePos());
+}
