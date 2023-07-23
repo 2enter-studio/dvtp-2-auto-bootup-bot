3534bdbafb333a8d4cb7b56340b219c20cb141cd
davilivob <david.iii.chiu@gmail.com>
2023-07-23 10:56:11 +0800

    Update from kdmofa

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,18 @@ import { open_screenshot_node_server, open_ue5 } from './lib/run-command.js';
 import 'dotenv/config'
 import dns from 'dns'
 
-const arena_pos_list = (process.env.ARENA_ICON_POS as string).split(',').map((x) => parseInt(x));
-const arena_icon_pos = { x: arena_pos_list[0], y: arena_pos_list[1] };
+function get_icon_pos(name: string) {
+  const pos_list = (process.env[name] as string).split(',').map((x) => parseInt(x));
+  return { x: pos_list[0], y: pos_list[1] };
+}
+
+const icon_pos = {
+  arena: get_icon_pos('ARENA_ICON_POS'),
+  ue5: get_icon_pos('UE5_ICON_POS'),
+  obs: get_icon_pos('OBS_ICON_POS'),
+  chrome: get_icon_pos('CHROME_ICON_POS'),
+  login: get_icon_pos('LOGIN_ICON_POS'),
+}
 
 robot.setKeyboardDelay(300);
 
@@ -41,12 +51,23 @@ function window_go_up() {
   key_binding(['command', 'up'], 1000)
 }
 
+function window_go_left() {
+  key_binding(['command', 'left'], 1000)
+}
+
 
 const { width, height } = robot.getScreenSize();
-async function click_desktop_icon(icon_pos: {x: number, y: number}) {
+
+async function click_desktop_icon(icon_pos: {x: number, y: number}, double_click=true) {
   await back_to_desktop();
   await sleep(1000);
   robot.moveMouse(icon_pos.x, icon_pos.y);
+  robot.mouseClick();
+  if(double_click) robot.mouseClick();
+}
+async function click_login_icon(icon_pos: {x: number, y: number}) {
+  robot.moveMouse(icon_pos.x, icon_pos.y);
+
   robot.mouseClick();
   robot.mouseClick();
 }
@@ -65,31 +86,46 @@ function get_mouse_pos() {
 
 async function main() {
   // Open Screenshot Server
+  await click_desktop_icon(icon_pos.chrome, false)
+  await sleep(3000);
+  robot.keyTap('tab')
+  robot.keyTap('tab')
+  robot.keyTap('tab')
+  robot.keyTap('enter')
+
+  await back_to_desktop();
+
   await open_screenshot_node_server();
   await sleep(10000)
 
-  // Open Unreal Engine 5 and play
-  await open_ue5();
-  await sleep(18000)
-  unreal_play();
-  await sleep(3000)
+  // Open Unreal Engine 5 
+  // await open_ue5();
+  await click_desktop_icon(icon_pos.obs)
+  await sleep(10000)
+  await click_desktop_icon(icon_pos.ue5)
+  await sleep(50000)
 
   // Open Arena
-  await click_desktop_icon(arena_icon_pos);
+  await click_desktop_icon(icon_pos.arena);
 
-  await sleep(3000);
-  // await switch_tab(5);
+  await sleep(10000);
+  await switch_tab(4);
+  
+  // Play UE5
+  await sleep(5000)
+  unreal_play();
+  await sleep(3000)
   // await sleep(10000);
 
   // Make Arena window left, and that'll make the ue5 window right, also focus on the later one.
-  window_go_up();
-  await sleep(1000);
-  robot.keyTap('enter');
+  //window_go_left();
+  //.await sleep(1000);
+  //robot.keyTap('enter');
   console.log('All done!')
 }
 
-// get_mouse_pos();
 const wait_until_connection = setInterval(async () => {
+//	get_mouse_pos();
   dns.resolve('dvtp2.2enter.art', function(err, addresses) {
     if (err) console.log(err);
     else {
--- src/lib/run-command.ts
+++ src/lib/run-command.ts
@@ -1,47 +1,47 @@
-import {spawn} from 'child_process';
+import { spawn } from 'child_process';
 import 'dotenv/config';
 
 const root_dir = process.env.PROJ_ROOT as string;
 
 export const open_ue5 = async () => {
-  const command = `${root_dir}\\\\ps-scripts\\\\open-ue5.ps1`
-  console.log(command)
-  const child = spawn("powershell.exe", [command]);
+	const command = `${root_dir}\\\\ps-scripts\\\\open-ue5.ps1`;
+	console.log(command);
+	const child = spawn('powershell.exe', [command]);
 
 
-  child.stdout.on("data",function(data){
-      console.log("Powershell Data: " + data);
-  });
+	child.stdout.on('data', function(data) {
+		console.log('Powershell Data: ' + data);
+	});
 
-  child.stderr.on("data",function(data){
-      console.log("Powershell Errors: " + data);
-  });
+	child.stderr.on('data', function(data) {
+		console.log('Powershell Errors: ' + data);
+	});
 
-  child.on("exit",function(){
-      console.log("Powershell Script finished");
-  });
+	child.on('exit', function() {
+		console.log('Powershell Script finished');
+	});
 
-  child.stdin.end(); //end input
+	child.stdin.end(); //end input
 
-}
+};
 
 export const open_screenshot_node_server = async () => {
-  const command = `${root_dir}\\\\ps-scripts\\\\open-screenshot-server.ps1`
-  console.log(command)
-  const child = spawn("powershell.exe", [command]);
+	const command = `${root_dir}\\\\ps-scripts\\\\open-screenshot-server.ps1`;
+	console.log(command);
+	const child = spawn('powershell.exe', [command]);
 
-  child.stdout.on("data",function(data){
-    console.log("Powershell Data: " + data);
-  });
+	child.stdout.on('data', function(data) {
+		console.log('Powershell Data: ' + data);
+	});
 
-  child.stderr.on("data",function(data){
-    console.log("Powershell Errors: " + data);
-  });
+	child.stderr.on('data', function(data) {
+		console.log('Powershell Errors: ' + data);
+	});
 
-  child.on("exit",function(){
-    console.log("Powershell Script finished");
-  });
+	child.on('exit', function() {
+		console.log('Powershell Script finished');
+	});
 
-  child.stdin.end(); //end input
+	child.stdin.end(); //end input
 
-}
+};
