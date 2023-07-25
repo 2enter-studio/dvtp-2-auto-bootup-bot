5280132ec7d4e562b3784cf587f4e624dee9f8b3
davilivob <david.iii.chiu@gmail.com>
2023-07-25 13:18:46 +0800

    Print location when it's invalid

--- src/lib/get_argv.ts
+++ src/lib/get_argv.ts
@@ -1,15 +1,23 @@
-import { hideBin } from 'yargs/helpers';
-import yargs from 'yargs/yargs';
-import { Argv } from 'yargs';
+import { hideBin } from "yargs/helpers";
+import yargs from "yargs/yargs";
+import { Argv } from "yargs";
 
-const modes = ['kdmofa', 'kdmofa-vr', 'kdmofa-cctv', 'moca-vr', 'moca', 'linz', 'linz-vr'];
+const modes = [
+	"kdmofa",
+	"kdmofa-vr",
+	"kdmofa-cctv",
+	"moca-vr",
+	"moca",
+	"linz",
+	"linz-vr",
+];
 
 export default () => {
 	const argv = yargs(hideBin(process.argv)).argv;
 	// @ts-ignore
 	const { mode } = argv;
 	if (!modes.includes(mode)) {
-		console.log('Invalid location');
+		console.log(`Invalid location: ${mode}`);
 		process.exit(1);
 	} else {
 		console.log(mode);
